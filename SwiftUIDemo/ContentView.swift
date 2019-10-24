36cd3e15008002ebeb23ec9a9251472b79c806b7
Subhra Roy <subhra.roy@e-arc.com>
2019-10-24 18:24:39 +0530

    Swift UI project added

---
+++ SwiftUIDemo/ContentView.swift
@@ -0,0 +1,48 @@
+//
+//  ContentView.swift
+//  SwiftUIDemo
+//
+//  Created by Subhra Roy on 24/10/19.
+//  Copyright © 2019 Subhra Roy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            ARCMapView()
+                .edgesIgnoringSafeArea(.top)
+                .frame(height: 300)
+            ARCLogoImageView()
+                .offset(y : -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .center) {
+                Text("Hello ARC Document")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(.gray)
+                    .multilineTextAlignment(.center)
+                HStack(alignment: .center) {
+                    Text("Salt Lake, Sector V")
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                    Spacer()
+                    Text("Kolkata - 700091")
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                }.padding()
+                
+                Spacer()
+                
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

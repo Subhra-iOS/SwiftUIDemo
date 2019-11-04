e95dce65a5752e80700ce3a82210246e6d71f9ee
Subhra Roy <subhra.roy@e-arc.com>
2019-11-04 14:35:48 +0530

    Fav toggle

--- SwiftUIDemo/LandmarkDetail.swift
+++ SwiftUIDemo/LandmarkDetail.swift
@@ -12,6 +12,10 @@ struct LandmarkDetail: View {
     @EnvironmentObject var userData: UserData
     var landmark : Landmark
     
+    var landmarkIndex: Int {
+        userData.landmarks.firstIndex(where: { $0.id == landmark.id })!
+    }
+    
     var body: some View {
         VStack {
             ARCMapView(coordinate: landmark.locationCoordinate)
@@ -21,19 +25,33 @@ struct LandmarkDetail: View {
                 .offset(y : -130)
                 .padding(.bottom, -130)
             
-            VStack(alignment: .center) {
-                Text(landmark.park)
-                    .font(.largeTitle)
+            VStack(alignment: .leading) {
+                HStack{
+                    Text(landmark.name)
+                    .font(.title)
                     .fontWeight(.bold)
                     .foregroundColor(.gray)
-                    .multilineTextAlignment(.center)
-                HStack(alignment: .center) {
+                    
+                    Button(action: {
+                        self.userData.landmarks[self.landmarkIndex].isFavorite.toggle()
+                    }) {
+                        if  self.userData.landmarks[self.landmarkIndex].isFavorite {
+                            Image(systemName: "star.fill")
+                            .foregroundColor(Color.yellow)
+                        }else{
+                            Image(systemName: "star")
+                            .foregroundColor(Color.gray)
+                        }
+                    }
+                    
+                }
+                HStack(alignment: .top) {
                     Text(landmark.park)
-                        .font(.subheadline)
+                        .font(.caption)
                         .foregroundColor(.gray)
                     Spacer(minLength: 10.0)
                     Text(landmark.state)
-                        .font(.subheadline)
+                        .font(.caption)
                         .foregroundColor(.gray)
                 }.padding()
                 
